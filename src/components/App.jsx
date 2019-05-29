207d07d6a6038bcf036f7ebfac7813b269472dcf
Lucian Covic <lucc@Lucs-MacBook-Pro-2.local>
2019-05-29 13:10:10 -0400

    started to add pagination functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,36 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';          
 import Nav from './Nav';
 import SearchBox from './SearchBox';
 import MovieList from './MovieList';
 
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super()
     this.state = {
       isLoaded: true,
       movies: [],
-      searchTerm: ''
+      searchTerm: '',
+      totalResults: 0,
+      currentPage: 1
     }
   }
 
+   window.API_KEY = `${process.env.REACT_APP_API}`;
+
 
   handleSubmit = (e) => {
-    const API_KEY = `${process.env.REACT_APP_API}`;
     e.preventDefault();
 
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${this.state.searchTerm}`)
       .then(res => res.json())
       .then(res => {
         console.log(res);
-        this.setState({ movies: [...res.results] })
+        this.setState({ 
+          movies: [...res.results],
+          totalResults: res.total_results  
+        })
       })
       .catch((err) => console.log('parsing failed', err));
   }
@@ -32,7 +39,22 @@ class App extends Component {
     this.setState({ searchTerm: e.target.value })    
   }
 
+  nextPage = (pageNumber) => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${this.state.searchTerm}&page=${pageNumber}`)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({
+        movies: [...res.results],
+        currentPage: pageNumber
+      })
+    })
+  }
+
   render() {
+    if(!this.state.isLoaded) {
+      return <div>LOADING...</div>
+    }
+
     return (
       <div className="App">
         <Nav />
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 const Movie = (props) => {
 	return(
 		<div>
-			{
-				props.image == null ? <img src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt="card" style={{ width: "25%", height: "360px" }} /> :
-					<img src={`https://image.tmdb.org/t/p/w185${props.image}`} alt="card" style={{ width: "25%", height: "360px" }}/>
-			}
-			<img src="" alt=""/>
+			<div>
+				{
+					props.image == null ? <img src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt="card" style={{ width: "25%", height: "360px" }} /> :
+						<img src={`https://image.tmdb.org/t/p/w185${props.image}`} alt="card" style={{ width: "25%", height: "360px" }}/>
+				}
+			</div>
+			<div className="card-content">
+				<p><a href="#">View Details</a></p>
+			</div>
 		</div>
 	)
 }
