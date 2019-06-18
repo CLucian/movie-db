b4bc60ea003f5761cc57f5bdd0131d5497cf348d
Lucian <lucian.covic@gmail.com>
2019-06-18 19:02:53 -0400

    Added default sort by top popularity index in descending order

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 import Movie from './Movie';
 
 const MovieList = (props) => {
+
+	const movieData = [].concat(props.movies)
+		.sort((a, b) => b.popularity - a.popularity)
+		.map((movie, i) => 
+			<div>
+				<Movie key={i} popIndex={movie.popularity} movieTitle={movie.original_title} posterImage={movie.poster_path} backdropImage={movie.backdrop_path} />
+			</div>
+		)
+	console.log(movieData)
 	return(
 		<div className="container">
-				{
-					props.movies.map((movie, i) => {
-						return (
-							<div className="container">
-								<div className="__row">
-									<Movie key={i} popIndex={movie.popularity} movieTitle={movie.original_title} posterImage={movie.poster_path} backdropImage={movie.backdrop_path} />
-								</div>
-							</div>
-						)
-					})
-				}
+				{ movieData }
 		</div>
 	)
 }
 
 export default MovieList;
+
+// props.movies.map((movie, i) => {
+// 	return (
+// 		<div className="container">
+// 			<Movie key={i} popIndex={movie.popularity} movieTitle={movie.original_title} posterImage={movie.poster_path} backdropImage={movie.backdrop_path} />
+// 		</div>
+// 	)
+// })
