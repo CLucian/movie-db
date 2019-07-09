740d74fa4c74ba4b7a3c55f5daec83404f5d55f5
Lucian <lucian.covic@gmail.com>
2019-07-09 19:41:07 -0400

    added svg back button

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -3,6 +3,8 @@ import { ReactComponent as Search } from "../img/SVG/search.svg";
 import { ReactComponent as Instagram } from "../img/SVG/instagram.svg";
 import { ReactComponent as Twitter } from "../img/SVG/twitter.svg";
 import { ReactComponent as Github } from "../img/SVG/github.svg";
+import { ReactComponent as Exit } from "../img/SVG/cross.svg";
+
 
 const MovieInfo = (props) => {
 	return (
@@ -10,13 +12,15 @@ const MovieInfo = (props) => {
 			<div className="backdrop">
 				<img className="backdrop--movieBackground" src={`https://image.tmdb.org/t/p/original${props.currentMovie.backdrop_path}`} />
 			</div>
-			<div className="row go-back" onClick={props.closeMovieInfo}>
-				<span className="goback">Go Back</span>
+			<div className="goback" onClick={props.closeMovieInfo}>
+				<div className="exit">
+					<Exit className="exit-svg" />
+				</div>
 			</div>
 			<div className="row-info">
 				<div className="containerInfo">
 					<div className="containerInfo--picture">
-					{props.currentMovie.poster_path === null ? <img src={"https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg"}/> :
+						{props.currentMovie.poster_path === null ? <img src={"https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg"}/> :
 						<img src={`https://image.tmdb.org/t/p/w342${props.currentMovie.poster_path}`} /> }
 					</div>
 				
@@ -26,7 +30,7 @@ const MovieInfo = (props) => {
 							<p className="infoTitle">Overview:</p>
 							<p className="info">{props.currentMovie.overview}</p>
 							<p className="infoTitle">Vote Average</p>
-							<p className="info">{props.currentMovie.vote_average}/10</p>
+							<p className="info">{props.currentMovie.vote_average} / 10</p>
 							<p className="infoTitle">Release Date</p>
 							<p className="info">{props.currentMovie.release_date.substring(5).split("-").concat(props.currentMovie.release_date.substring(0, 4)).join("/")}</p>
 						</div>
