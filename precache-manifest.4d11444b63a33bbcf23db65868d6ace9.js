91a2124738d2caade692b95e839d89e3a8193352
Lucian <lucian.covic@gmail.com>
2020-08-04 18:47:48 -0400

    Updates

---
+++ precache-manifest.4d11444b63a33bbcf23db65868d6ace9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0429fbe66b1c33753fa480243a984e",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "45b6bdb6ae66bd7a60e7",
+    "url": "/movie-db/static/css/main.c3910ce3.chunk.css"
+  },
+  {
+    "revision": "1202c9c43c4bd4f599b7",
+    "url": "/movie-db/static/js/2.54515ec5.chunk.js"
+  },
+  {
+    "revision": "45b6bdb6ae66bd7a60e7",
+    "url": "/movie-db/static/js/main.36e4297a.chunk.js"
+  },
+  {
+    "revision": "6bb80631b382c7965b33",
+    "url": "/movie-db/static/js/runtime~main.5114c00d.js"
+  },
+  {
+    "revision": "58ecf6928a5d4f9ac4f24e5ca383fd63",
+    "url": "/movie-db/static/media/Sheen.58ecf692.png"
+  },
+  {
+    "revision": "6fa59054c3db6e2ae3aa67a24dca2c94",
+    "url": "/movie-db/static/media/comedy.6fa59054.png"
+  },
+  {
+    "revision": "91159e72a2d395337fce134bc6afd7d1",
+    "url": "/movie-db/static/media/cross.91159e72.svg"
+  },
+  {
+    "revision": "1fc7ff0b941570453667b57847926e4c",
+    "url": "/movie-db/static/media/github.1fc7ff0b.svg"
+  },
+  {
+    "revision": "a626983f670fffde777a60e4cece8214",
+    "url": "/movie-db/static/media/imageTheatre.a626983f.jpg"
+  },
+  {
+    "revision": "c057d500fd72706e52f87b057eaeb883",
+    "url": "/movie-db/static/media/instagram.c057d500.svg"
+  },
+  {
+    "revision": "42534d318988c3241e7225fdb0905b44",
+    "url": "/movie-db/static/media/leo.42534d31.png"
+  },
+  {
+    "revision": "bf3113cdf3a30207d9a5399b7aea0f0b",
+    "url": "/movie-db/static/media/search.bf3113cd.svg"
+  },
+  {
+    "revision": "6134b234fbe588b9dc309b2bd14caa35",
+    "url": "/movie-db/static/media/twitter.6134b234.svg"
+  },
+  {
+    "revision": "2ddd09ebc3dac15dd8173624e8ae8420",
+    "url": "/movie-db/static/media/war.2ddd09eb.png"
+  }
+]);
