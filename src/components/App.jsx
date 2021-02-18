23bfc0b8335f3ffe9a294f111761bf713659fbb0
Lucian <lucian.covic@gmail.com>
2021-02-18 13:32:40 -0500

    removed some console logs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,17 @@
 // import React, { Component } from 'react';
-import React from 'react';          
+import React from "react";
 
 // component imports
-import DropDown from './DropDown';
-import MovieList from './MovieList';
-import Pagination from './Pagination';
-import Pages from './Pages';
-import SearchBox from './SearchBox';
-import MovieInfo from './MovieInfo';
+import DropDown from "./DropDown";
+import MovieList from "./MovieList";
+import Pagination from "./Pagination";
+import Pages from "./Pages";
+import SearchBox from "./SearchBox";
+import MovieInfo from "./MovieInfo";
 // import Movie from './Movie';
 
-
 // styles imports
-import '../sass/main.scss';
-
-
+import "../sass/main.scss";
 
 // url import
 import {
@@ -22,27 +19,22 @@ import {
   constructSearchURL,
   constructGenreURL,
   constructMovieIdURL,
-  constructSimilarMovieIdURL
-} from '../utils/url'
- 
-import {
-  isGenre,
-  isCategory
-} from '../utils/helpers'
+  constructSimilarMovieIdURL,
+} from "../utils/url";
 
-
+import { isGenre, isCategory } from "../utils/helpers";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.resultsType = ''
+    this.resultsType = "";
 
     this.state = {
       isLoaded: true,
       movies: [],
       movie: null,
-      searchTerm: '',
+      searchTerm: "",
       totalPages: null,
       totalResults: 0,
       pageNum: 1,
@@ -51,107 +43,100 @@ class App extends React.Component {
       popIndex: 5,
       currentMovie: null,
       similarMovies: [],
-      movieTrailer: []
-    }
+      movieTrailer: [],
+    };
 
     // GENRE
     // Creates a new function by binding the context (this) and argument (genre/category/pageNum) to a function
-
 
     // PAGE NUMBER / DIRECTIONAL
     this.nextPage = this.changePage.bind(this, 1);
     this.prevPage = this.changePage.bind(this, -1);
 
-
     // GENRE
-    this.fetchAction = this.fetchGenre.bind(this, 'action')
-    this.handleAction = this.handleGenre.bind(this, 'action')
+    this.fetchAction = this.fetchGenre.bind(this, "action");
+    this.handleAction = this.handleGenre.bind(this, "action");
 
-    this.fetchAdventure = this.fetchGenre.bind(this, 'adventure')
-    this.handleAdventure = this.handleGenre.bind(this, 'adventure')
+    this.fetchAdventure = this.fetchGenre.bind(this, "adventure");
+    this.handleAdventure = this.handleGenre.bind(this, "adventure");
 
-    this.fetchComedy = this.fetchGenre.bind(this, 'comedy')
-    this.handleComedy = this.handleGenre.bind(this, 'comedy')
-    
-    this.fetchCrime = this.fetchGenre.bind(this, 'crime')
-    this.handleCrime = this.handleGenre.bind(this, 'crime')
+    this.fetchComedy = this.fetchGenre.bind(this, "comedy");
+    this.handleComedy = this.handleGenre.bind(this, "comedy");
 
-    this.fetchRomance = this.fetchGenre.bind(this, 'romance')
-    this.handleRomance = this.handleGenre.bind(this, 'romance')
+    this.fetchCrime = this.fetchGenre.bind(this, "crime");
+    this.handleCrime = this.handleGenre.bind(this, "crime");
 
-    this.fetchHorror = this.fetchGenre.bind(this, 'horror')
-    this.handleHorror = this.handleGenre.bind(this, 'horror')
+    this.fetchRomance = this.fetchGenre.bind(this, "romance");
+    this.handleRomance = this.handleGenre.bind(this, "romance");
 
-    this.fetchSciFi = this.fetchGenre.bind(this, 'scifi')
-    this.handleSciFi = this.handleGenre.bind(this, 'scifi')
+    this.fetchHorror = this.fetchGenre.bind(this, "horror");
+    this.handleHorror = this.handleGenre.bind(this, "horror");
 
-    this.fetchWar = this.fetchGenre.bind(this, 'war')
-    this.handleWar = this.handleGenre.bind(this, 'war')
+    this.fetchSciFi = this.fetchGenre.bind(this, "scifi");
+    this.handleSciFi = this.handleGenre.bind(this, "scifi");
+
+    this.fetchWar = this.fetchGenre.bind(this, "war");
+    this.handleWar = this.handleGenre.bind(this, "war");
 
     // CATEGORY
-    this.fetchNowPlaying = this.fetchCategory.bind(this, 'nowPlaying')
-    this.handleNowPlaying = this.handleCategory.bind(this, 'nowPlaying')
+    this.fetchNowPlaying = this.fetchCategory.bind(this, "nowPlaying");
+    this.handleNowPlaying = this.handleCategory.bind(this, "nowPlaying");
 
-    this.fetchTopRated = this.fetchCategory.bind(this, 'topRated')
-    this.handleTopRated = this.handleCategory.bind(this, 'topRated')
+    this.fetchTopRated = this.fetchCategory.bind(this, "topRated");
+    this.handleTopRated = this.handleCategory.bind(this, "topRated");
 
-    this.fetchTrending = this.fetchCategory.bind(this, 'trending')
-    this.handleTrending = this.handleCategory.bind(this, 'trending')
-
-
+    this.fetchTrending = this.fetchCategory.bind(this, "trending");
+    this.handleTrending = this.handleCategory.bind(this, "trending");
   }
-
-
-
 
   fetchGenre = (genre) => {
     this.resultsType = genre;
 
-    const url = constructGenreURL(genre, this.state.pageNum)
+    const url = constructGenreURL(genre, this.state.pageNum);
 
     fetch(url)
-      .then(data => data.json())
-      .then(data => {
+      .then((data) => data.json())
+      .then((data) => {
         this.setState({
           movies: data.results,
-          totalPages: data.total_pages
-        })
-      })
-  }
+          totalPages: data.total_pages,
+        });
+      });
+  };
 
   handleGenre = (genre) => {
-    this.setState({ pageNum: 1, searchTerm: '' }, () => this.fetchGenre(genre))
-  }
+    this.setState({ pageNum: 1, searchTerm: "" }, () => this.fetchGenre(genre));
+  };
 
   fetchCategory = (category) => {
     this.resultsType = category;
 
-    const url = getCategoryURL(category, this.state.pageNum)
+    const url = getCategoryURL(category, this.state.pageNum);
 
     fetch(url)
-      .then(data => data.json())
-      .then(data => {
+      .then((data) => data.json())
+      .then((data) => {
         this.setState({
           movies: data.results,
-          totalPages: data.total_pages
-        })
-      })
-  }
+          totalPages: data.total_pages,
+        });
+      });
+  };
 
   handleCategory = (category) => {
-    this.setState({ pageNum: 1, searchTerm: '' }, () => this.fetchCategory(category))
-  }
-
+    this.setState({ pageNum: 1, searchTerm: "" }, () =>
+      this.fetchCategory(category)
+    );
+  };
 
   ///////////////////////////////
   // API FETCHING
   ///////////////////////////////
-  
 
   // Sets the searchTerm state to what is typed in the input value
   handleChange = (e) => {
     this.setState({ searchTerm: e.target.value });
-  }
+  };
 
   // Calls fetchMovies when the search term is submitted
   handleSubmit = (e) => {
@@ -161,23 +146,21 @@ class App extends React.Component {
       // Calls fetchMovie API fetch for typed search term
       this.fetchMovies(this.state.searchTerm);
     });
-  }
-
+  };
 
   // API fetch to TMDB, sets movie state and total pages to API results
   fetchMovies = (search) => {
     this.resultsType = search;
 
-    const url = constructSearchURL(search, this.state.pageNum)
+    const url = constructSearchURL(search, this.state.pageNum);
     fetch(url)
-      .then(res => res.json())
-      .then(json => {
-        console.log(json);
+      .then((res) => res.json())
+      .then((json) => {
         if (json.results) {
           this.setState({
             movies: json.results,
-            totalPages: json.total_pages
-          })
+            totalPages: json.total_pages,
+          });
         }
 
         // Just in case an JSON does not provide a results field
@@ -185,127 +168,121 @@ class App extends React.Component {
           this.setState({ movies: [] });
         }
       })
-      .catch((err) => console.log('PARSING FAILURE', err));
-  }
+      .catch((err) => console.log("PARSING FAILURE", err));
+  };
 
-  
   // Loads fetchNowPlaying movies when default page loads
-  componentDidMount(){
+  componentDidMount() {
     this.fetchNowPlaying();
   }
-
 
   ///////////////////////////////
   // PAGE CHANGING
   ///////////////////////////////
 
   // dir = direction
-  
 
   changePage = (dir) => {
     let pageNumber = this.state.pageNum;
 
     // If no input --> return same page
     if (this.state.movies.length === 0) {
-      return 
-    } 
+      return;
+    }
     // If moving forward on pages, increases the page number by 1
-    if (dir === 1){
+    if (dir === 1) {
       pageNumber += 1;
-    // If moving backwards on pages AND page number is already greater than 1, then decreases page number by 1
+      // If moving backwards on pages AND page number is already greater than 1, then decreases page number by 1
     } else if (dir === -1 && pageNumber > 1) {
       pageNumber -= 1;
     }
     // Afterwards updates state and runs the handleGenreOrCategory function
-    this.setState({
-      pageNum: pageNumber
-    }, this.handleGenreOrCategory)
-  }
+    this.setState(
+      {
+        pageNum: pageNumber,
+      },
+      this.handleGenreOrCategory
+    );
+  };
 
   handleGenreOrCategory() {
     // Communicates with helper function isGenre (in helpers.js) to determine if this.resultsType includes a genre in the isGenre function and runs fetchGenre if so
     if (isGenre(this.resultsType)) {
-      this.fetchGenre(this.resultsType)
-    // Communicates with helper function isCategory (in helpers.js) to determine if this.resultsType includes a genre in the isCategory function and runs fetchCategory if so
+      this.fetchGenre(this.resultsType);
+      // Communicates with helper function isCategory (in helpers.js) to determine if this.resultsType includes a genre in the isCategory function and runs fetchCategory if so
     } else if (isCategory(this.resultsType)) {
-      this.fetchCategory(this.resultsType)
-    // If none of the functions above are satisfied, will fetchMovies with search resultsType (in case of error)
+      this.fetchCategory(this.resultsType);
+      // If none of the functions above are satisfied, will fetchMovies with search resultsType (in case of error)
     } else {
-      this.fetchMovies(this.resultsType)
+      this.fetchMovies(this.resultsType);
     }
   }
-
 
   ///////////////////////////////
   // PAGINATION
   ///////////////////////////////
 
   pagination = (pageNumber) => {
-    this.setState({
-      pageNum: pageNumber
-    }, this.handleGenreOrCategory)
-  }
+    this.setState(
+      {
+        pageNum: pageNumber,
+      },
+      this.handleGenreOrCategory
+    );
+  };
 
   ///////////////////////////////
   // MOVIE OVERVIEW
   ///////////////////////////////x
 
-
   setMovieInfo = (id) => {
-    this.fetchMovieId(id)
-  }
-
+    this.fetchMovieId(id);
+  };
 
   // fetches API from url in url file (constructMovieIdURL) which takes in the ID of a specific movie, then sets movie state to result of movie from API
   fetchMovieId = (id) => {
-    const url = constructMovieIdURL(id)
+    const url = constructMovieIdURL(id);
 
     fetch(url)
-      .then(res => res.json())
-      .then(json => {
+      .then((res) => res.json())
+      .then((json) => {
         this.setState({
-          movie: json
-        })
-      })
-  }
+          movie: json,
+        });
+      });
+  };
 
   closeMovieInfo = () => {
-    this.setState({ movie: null })
-  }
+    this.setState({ movie: null });
+  };
 
   fetchSimilarMovieId = (id) => {
     const url = constructSimilarMovieIdURL(id);
 
     fetch(url)
-    .then(res => res.json())
-      .then(json => {
-        console.log(json)
+      .then((res) => res.json())
+      .then((json) => {
         this.setState({
-          similarMovies: json.results
-        })
-      })
-  }
+          similarMovies: json.results,
+        });
+      });
+  };
 
   setSimilarMovieInfo = (id) => {
-    this.fetchSimilarMovieId(id)
-  }
-
-
+    this.fetchSimilarMovieId(id);
+  };
 
   ///////////////////////////////
   // RENDER
   ///////////////////////////////
 
   render() {
-    if(!this.state.isLoaded) {
-      return <div>LOADING...</div>
+    if (!this.state.isLoaded) {
+      return <div>LOADING...</div>;
     }
 
-
     return (
-
-
-      <div className={`App ${this.state.movie && 'some-class'}`}>
+      <div className={`App ${this.state.movie && "some-class"}`}>
         {/* example error handling */}
         {/* this.state.customError && <div>
             Error!
@@ -313,8 +290,8 @@ class App extends React.Component {
             {this.state.customError}
             <button onClick={() => this.setState({customError: null})}>Close</button>
         </div> */}
-      
-        {this.state.movie === null ? 
+
+        {this.state.movie === null ? (
           <div>
             <DropDown
               handleNowPlaying={this.handleNowPlaying}
@@ -328,7 +305,7 @@ class App extends React.Component {
               handleSciFi={this.handleSciFi}
               handleWar={this.handleWar}
             />
-          
+
             <SearchBox
               handleSubmit={this.handleSubmit}
               handleChange={this.handleChange}
@@ -344,22 +321,35 @@ class App extends React.Component {
               handleRomance={this.handleRomance}
               handleSciFi={this.handleSciFi}
               handleWar={this.handleWar}
-            /> 
-            <MovieList  movies={this.state.movies} setMovieInfo={this.setMovieInfo} similarMovies={this.state.similarMovies} setSimilarMovieInfo={this.setSimilarMovieInfo} />
+            />
+            <MovieList
+              movies={this.state.movies}
+              setMovieInfo={this.setMovieInfo}
+              similarMovies={this.state.similarMovies}
+              setSimilarMovieInfo={this.setSimilarMovieInfo}
+            />
           </div>
-          :
-          <MovieInfo closeMovieInfo={this.closeMovieInfo} currentMovie={this.state.movie} setMovieInfo={this.setMovieInfo} similarMovies={this.state.similarMovies} setSimilarMovieInfo={this.setSimilarMovieInfo} />
-        }
+        ) : (
+          <MovieInfo
+            closeMovieInfo={this.closeMovieInfo}
+            currentMovie={this.state.movie}
+            setMovieInfo={this.setMovieInfo}
+            similarMovies={this.state.similarMovies}
+            setSimilarMovieInfo={this.setSimilarMovieInfo}
+          />
+        )}
 
-
-        { this.state.totalPages > 1 && this.state.movie === null ? 
-          <Pagination 
-            movies={this.state.movies} 
-            totalPages={this.state.totalPages} 
-            pagination={this.pagination} 
-            pageNum={this.state.pageNum} />
-           : '' }
-        { this.state.totalPages > 1 && this.state.movie === null ?
+        {this.state.totalPages > 1 && this.state.movie === null ? (
+          <Pagination
+            movies={this.state.movies}
+            totalPages={this.state.totalPages}
+            pagination={this.pagination}
+            pageNum={this.state.pageNum}
+          />
+        ) : (
+          ""
+        )}
+        {this.state.totalPages > 1 && this.state.movie === null ? (
           <Pages
             nextPage={this.nextPage}
             prevPage={this.prevPage}
@@ -367,7 +357,9 @@ class App extends React.Component {
             totalPages={this.state.totalPages}
             movies={this.state.movies}
           />
-          : '' }
+        ) : (
+          ""
+        )}
       </div>
     );
   }
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
-
-
+import React from "react";
 
 const Movie = (props) => {
+  const { title, id, poster_path, release_date } = props.movie;
 
-	const { title, id, poster_path, release_date } = props.movie
-	
-	console.log('THIS IS THE RELEASE DATE', release_date)
-	return(
-			<div className="movie-card">
-				<div>
-					<div onClick={() => {props.setMovieInfo(id); props.setSimilarMovieInfo(id)}}>
-							{poster_path == null ?
-								<img src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} className="movie-thumbnail" alt="card" style={{ width: "185px", height: "278px" }} />
-								:
-								<img src={`https://image.tmdb.org/t/p/w185${poster_path}`} className="movie-thumbnail" alt="card" />
-							}
+  return (
+    <div className="movie-card">
+      <div>
+        <div
+          onClick={() => {
+            props.setMovieInfo(id);
+            props.setSimilarMovieInfo(id);
+          }}
+        >
+          {poster_path == null ? (
+            <img
+              src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`}
+              className="movie-thumbnail"
+              alt="card"
+              style={{ width: "185px", height: "278px" }}
+            />
+          ) : (
+            <img
+              src={`https://image.tmdb.org/t/p/w185${poster_path}`}
+              className="movie-thumbnail"
+              alt="card"
+            />
+          )}
 
-							<h1 className="movieHeader">{title} <br/> { (release_date === "" || release_date === undefined ) ? null : `(${release_date.slice(0, 4)})` }</h1>
-					</div>
-				</div>
-			</div>
-		)
-}
+          <h1 className="movieHeader">
+            {title} <br />{" "}
+            {release_date === "" || release_date === undefined
+              ? null
+              : `(${release_date.slice(0, 4)})`}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Movie;
 
 // movie searches come out based off popularity
-
